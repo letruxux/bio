7f40518f0d61a27c44f2c2d42141fc0f5011ce8d
letruxux <mrgianfranco483@gmail.com>
2025-02-17 22:48:44 +0100

    order projects

--- src/app/p/page.tsx
+++ src/app/p/page.tsx
@@ -3,7 +3,7 @@
 import { projects } from "@/components/projects";
 import { useRef } from "react";
 import gsap from "gsap";
-import { ArrowLeft } from "lucide-react";
+import { AlignCenter, ArrowLeft } from "lucide-react";
 import { useGSAP } from "@gsap/react";
 
 export default function ProjectsList() {
@@ -38,30 +38,40 @@ export default function ProjectsList() {
       <div className="max-w-4xl mx-auto">
         <div ref={headerRef} className="mb-12 text-center">
           <h1 className="text-4xl font-bold mb-4">my projects :3</h1>
+          <h3 className="text-lg">
+            <AlignCenter className="inline" /> from newest to oldest
+          </h3>
         </div>
 
         <div ref={projectsRef} className="flex flex-col gap-6">
-          {Object.entries(projects).map(([key, project]) => (
-            <a href={`/p/${key}`} key={key}>
-              <div className="project-card bg-blue-bg rounded-xl border border-border-subtle hover:border-accent-primary transition-colors overflow-hidden">
-                {project.bannerImage && (
-                  <div
-                    className="h-48 w-full bg-cover bg-center"
-                    style={{ backgroundImage: `url("${project.bannerImage}")` }}
-                  />
-                )}
-                <div className="p-6">
-                  <div className="w-16 h-16 max-w-16 max-h-16 overflow-hidden bg-accent-primary rounded-xl flex items-center justify-center mb-4">
-                    <span className="scale-50">{project.icon}</span>
+          {Object.entries(projects)
+            .sort(([_, a], [__, b]) => {
+              const startTimeA = a.time.start;
+              const startTimeB = b.time.start;
+
+              return startTimeB.getTime() - startTimeA.getTime();
+            })
+            .map(([key, project]) => (
+              <a href={`/p/${key}`} key={key}>
+                <div className="project-card bg-blue-bg rounded-xl border border-border-subtle hover:border-accent-primary transition-colors overflow-hidden">
+                  {project.bannerImage && (
+                    <div
+                      className="h-48 w-full bg-cover bg-center"
+                      style={{ backgroundImage: `url("${project.bannerImage}")` }}
+                    />
+                  )}
+                  <div className="p-6">
+                    <div className="w-16 h-16 max-w-16 max-h-16 overflow-hidden bg-accent-primary rounded-xl flex items-center justify-center mb-4">
+                      <span className="scale-50">{project.icon}</span>
+                    </div>
+                    <h2 className="text-2xl font-bold mb-2">{project.title}</h2>
+                    <p className="text-text-secondary line-clamp-2">
+                      {project.description}
+                    </p>
                   </div>
-                  <h2 className="text-2xl font-bold mb-2">{project.title}</h2>
-                  <p className="text-text-secondary line-clamp-2">
-                    {project.description}
-                  </p>
                 </div>
-              </div>
-            </a>
-          ))}
+              </a>
+            ))}
         </div>
         <a
           className="flex items-center gap-2 bg-transparent bg-surface-light  px-6 py-3 rounded-lg transition-colors border border-transparent hover:border-border-subtle action-button mt-4 w-32 h-12"
