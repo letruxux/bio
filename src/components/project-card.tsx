5e7b9709e70e6e9f69956c7e7e285d1bdca7a074
letruxux <mrgianfranco483@gmail.com>
2025-02-19 16:12:00 +0100

    fix card date

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -56,7 +56,7 @@ export default function ProjectCard({
               />
             </div>
             <div className="text-sm text-text-secondary shrink-0">
-              {dayjs(project.timeline.at(0)!.date).fromNow()}
+              {dayjs(project.timeline.at(1)!.date).fromNow()}
             </div>
           </div>
         </div>
