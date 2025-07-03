fdbf6ccb4876957dc92b30ebb3e975af4ce073a8
riccardo <102257756+letruxux@users.noreply.github.com>
2025-07-03 12:37:27 +0200

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -7,7 +7,7 @@ const Card: React.FC<PropsOf<HTMLDivElement>> = ({ children, ...props }) => {
     <div
       {...props}
       className={twMerge(
-        "rounded-2xl p-6 text-text-primary shadow-lg border border-border-subtle",
+        "rounded-2xl p-6 md:p-4 sm:p-2 text-text-primary shadow-lg border border-border-subtle",
         "bg-gradient-to-br from-surface-light to-surface-deep",
         props.className
       )}
