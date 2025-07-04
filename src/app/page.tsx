41e598c2a340e26e436330131f1cc1623864318e
letruxux <mrgianfranco483@gmail.com>
2025-07-05 01:49:44 +0200

    Go go go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Lastfm from "@/components/lastfm";
 export default function Home() {
   return (
     <>
-      <main className="bg-gradient-to-br from-gray-900 via-black to-gray-800 px-8 min-h-dvh flex flex-col items-center justify-center">
+      <main className="bg-gradient-to-br from-gray-900 via-black to-gray-800 px-2 min-h-dvh flex flex-col items-center justify-center">
         <Card />
         <Lastfm />
       </main>
