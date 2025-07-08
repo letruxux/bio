52c10395b0a0c4c7733beb031c7f3a9385833544
riccardo <102257756+letruxux@users.noreply.github.com>
2025-07-09 01:45:10 +0200

    Update gif.tsx

--- src/components/gif.tsx
+++ src/components/gif.tsx
@@ -6,11 +6,9 @@ export default function GifBox({ ref }: { ref: React.RefObject<HTMLDivElement | 
       <h3 className="text-2xl font-bold mb-4 text-text-primary">hello</h3>
       <Card className="p-4">
         <img
-          src="https://media1.tenor.com/m/cDJj3LEw0UIAAAAd/gmail-dog.gif"
-          alt="aaaaaaaaaaaaaaaaaaaaaaaa"
+          src="https://media2.giphy.com/media/v1.Y2lkPTZjMDliOTUyYWdsb2VydG1zd3JzaTlxOWltYnJ6dmNxMnlkZmgwOWR0M2lmNmRtaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/q1MeAPDDMb43K/giphy.gif"
+          alt="woo ..."
           className="rounded-2xl w-full h-auto"
-          width={399}
-          height={498}
         />
       </Card>
     </div>
